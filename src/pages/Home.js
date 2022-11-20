d91dfd173e9f4ca7b5ee70200878c31914238483
JosephJanoneTiwouw <113010008+JosephJanoneTiwouw@users.noreply.github.com>
2022-11-21 05:24:08 +0700

    Add files via upload

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+// home page
+const Home = () => {
+    return(
+        <div>
+            <h1>Hi Joseph Janone Tiwouw</h1>
+            <h3>Welcome to Universitas Klabat</h3>
+        </div>
+    );
+};
+
+export default Home;
